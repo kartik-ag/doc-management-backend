11e42f97fa6761300fed188ec2a2847a805f3489
Kartik Agarwal <agarwalkartik2018@gmail.com>
2025-05-25 22:33:21 +0530

    deployment changes

--- doc_management/settings.py
+++ doc_management/settings.py
@@ -12,7 +12,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+# Get the allowed hosts from environment variable, with a default for development
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 INSTALLED_APPS = [
