b191a3168774b85d70889ab2a1d6b3d0ab0ba0e1
Kartik Agarwal <agarwalkartik2018@gmail.com>
2025-05-25 23:13:03 +0530

    login bug removed

--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    # Use email for authentication
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
